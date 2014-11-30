720e33752f57e994b4733f195f304864c65b3459
jholdstock <jam.jar69@gmail.com>
2014-11-30 20:38:42 +0000

    parse build datetime. show status in bottom left

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var createOrUpdateElement = function (id, wrapper, topLeftText, topRightText, bottomLeftText, color) {
+var createOrUpdateElement = function (id, wrapper, topLeftText, topRightText, bottomLeftText, bottomRightText, color) {
   var existingElement = $("div#" + id);
 
   if (existingElement.length > 0) {
@@ -6,19 +6,22 @@ var createOrUpdateElement = function (id, wrapper, topLeftText, topRightText, bo
     var topLeft = $("div.tsm_topLeft", existingElement);
     var topRight = $("div.tsm_topRight", existingElement);
     var bottomLeft = $("div.tsm_bottomLeft", existingElement);
+    var bottomRight = $("div.tsm_bottomRight", existingElement);
   }
   else {
     var bgElement = $("<div>").attr("id", id);
     var topLeft = $("<div>").addClass("tsm_topLeft");
     var topRight = $("<div>").addClass("tsm_topRight");
     var bottomLeft = $("<div>").addClass("tsm_bottomLeft");
-    bgElement.append(topLeft).append(bottomLeft).append(topRight);
+    var bottomRight = $("<div>").addClass("tsm_bottomRight");
+    bgElement.append(topLeft).append(topRight).append(bottomLeft).append(bottomRight);
     wrapper.append(bgElement);
   }
 
   topLeft.html(topLeftText);
   topRight.html(topRightText);
   bottomLeft.html(bottomLeftText);
+  bottomRight.html(bottomRightText);
 
   bgElement.removeClass().addClass(color);
 }
@@ -39,16 +42,31 @@ var drawAgent = function(agent) {
   var topLeft = agent.name;
   var topRight = agent.connected? "Connected" : "Disconnected";
   var bottomLeft = agent.enabled? "Enabled" : "Disabled";
-  createOrUpdateElement("tsm_a_" + agent.id, $(".tsm_agent_wrapper"), topLeft, topRight, bottomLeft, color)
+  createOrUpdateElement("tsm_a_" + agent.id, $(".tsm_agent_wrapper"), topLeft, topRight, bottomLeft, "", color)
 };
 
 var drawBuild = function(buildType) {
   return function(build) {
     if (build.status != "SUCCESS") {
       var topLeft = buildType.projectName + " :: " + buildType.name;
-      var topRight = build.finishDate + "<br>" + build.statusText;
+      
+      var date = build.finishDate.substring(0,8);
+      var year = date.substring(0,4);
+      var month = date.substring(4,6);
+      var month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"][month-1];
+
+      var day = date.substring(6,8);
+      var finishDate = day + " " +  month + " " + year;
+      
+      var time = build.finishDate.substring(9,14);
+      var hours = time.substring(0,2);
+      var minutes = time.substring(2,4);
+      var finishTime = hours + ":" +  minutes;
+
+      var topRight = "Failed on " + finishDate + " at " + finishTime;
+
       var bottomLeft = build.status;
-      createOrUpdateElement("tsm_b_" + buildType.id, $("div.tsm_build_wrapper"), topLeft, topRight, bottomLeft, "tsm_red");
+      createOrUpdateElement("tsm_b_" + buildType.id, $("div.tsm_build_wrapper"), topLeft, topRight, bottomLeft, build.statusText, "tsm_red");
     }
     else {
       $("div#tsm_b_" + buildType.id).remove();
