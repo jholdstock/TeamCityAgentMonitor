ce00799dce5218e3040ad07169af44e2b08f591e
jholdstock <jam.jar69@gmail.com>
2014-11-30 13:57:41 +0000

    live updates of agent status

--- js/main.js
+++ js/main.js
@@ -1,4 +1,21 @@
 var drawAgent = function(agent) {
+  var agentElement = $("div#tsm_a_" + agent.id);
+
+  if (agentElement.length > 0) {
+    var bgElement = agentElement;
+    var name = $("div.tsm_topLeft", agentElement);
+    var connected = $("div.tsm_topRight", agentElement);
+    var enabled = $("div.tsm_bottomLeft", agentElement);
+  }
+  else {
+    var bgElement = $("<div>").attr("id", "tsm_a_" + agent.id);
+    var name = $("<div>").addClass("tsm_topLeft");
+    var connected = $("<div>").addClass("tsm_topRight");
+    var enabled = $("<div>").addClass("tsm_bottomLeft");
+    bgElement.append(name).append(enabled).append(connected);
+    $("div.tsm_wrapper").append(bgElement);
+  }
+
   var color;
   if (agent.connected) {
     if (agent.enabled) {
@@ -12,16 +29,10 @@ var drawAgent = function(agent) {
     color = "tsm_gray";
   }
 
-  agent.connected = agent.connected? "Connected" : "Disconnected";
-  agent.enabled = agent.enabled? "Enabled" : "Disabled";
-
-  var name = $("<div>").html(agent.name).addClass("tsm_topLeft");
-  var connected = $("<div>").html(agent.connected).addClass("tsm_topRight");
-  var enabled = $("<div>").html(agent.enabled).addClass("tsm_bottomLeft");
-
-  var bgElement = $("<div>").addClass(color);
-  bgElement.append(name).append(enabled).append(connected);
-  $("div.tsm_wrapper").append(bgElement);
+  name.html(agent.name);
+  connected.html(agent.connected? "Connected" : "Disconnected");
+  enabled.html(agent.enabled? "Enabled" : "Disabled");
+  bgElement.removeClass().addClass(color);
 };
 
 var agentIdCallback = function(response) {
@@ -30,21 +41,27 @@ var agentIdCallback = function(response) {
   for (var i = 0; i< agentIds.length; i++) {
     var agentId = agentIds[i].id;
     $.ajax({
-      url: "http://lon1vci01.int.openbet.com/httpAuth/app/rest/agents/id:" + agentId + "",
+      url: tcUrl + "/httpAuth/app/rest/agents/id:" + agentId + "",
       headers: { Accept:"application/json" },
       success: drawAgent
     });
   }
 }
 
+var downloadAndDisplayAgents = function(url) {
+  $.ajax({
+    url: tcUrl + "/httpAuth/app/rest/agents",
+    headers: { Accept:"application/json" },
+    success: agentIdCallback
+  });
+  setTimeout(downloadAndDisplayAgents, 2000);
+}
+
+var tcUrl = "http://lon1vci01.int.openbet.com";
 var body = $("body");
 body.empty();
 $("<div>").addClass("tsm_wrapper").appendTo(body);
 
 chrome.runtime.sendMessage({}, function(response) {
-  $.ajax({
-    url: "http://lon1vci01.int.openbet.com/httpAuth/app/rest/agents",
-    headers: { Accept:"application/json" },
-    success: agentIdCallback
-  });
+  downloadAndDisplayAgents();
 });
