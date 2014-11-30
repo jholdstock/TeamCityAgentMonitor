3edee1e762ed4eb9944032ffcb479eb788d5bee9
jholdstock <jam.jar69@gmail.com>
2014-11-30 19:58:18 +0000

    show failed build details. hide when passing.

--- js/main.js
+++ js/main.js
@@ -1,21 +1,29 @@
-var drawAgent = function(agent) {
-  var agentElement = $("div#tsm_a_" + agent.id);
+var createOrUpdateElement = function (id, wrapper, topLeftText, topRightText, bottomLeftText, color) {
+  var existingElement = $("div#" + id);
 
-  if (agentElement.length > 0) {
-    var bgElement = agentElement;
-    var name = $("div.tsm_topLeft", agentElement);
-    var connected = $("div.tsm_topRight", agentElement);
-    var enabled = $("div.tsm_bottomLeft", agentElement);
+  if (existingElement.length > 0) {
+    var bgElement = existingElement;
+    var topLeft = $("div.tsm_topLeft", existingElement);
+    var topRight = $("div.tsm_topRight", existingElement);
+    var bottomLeft = $("div.tsm_bottomLeft", existingElement);
   }
   else {
-    var bgElement = $("<div>").attr("id", "tsm_a_" + agent.id);
-    var name = $("<div>").addClass("tsm_topLeft");
-    var connected = $("<div>").addClass("tsm_topRight");
-    var enabled = $("<div>").addClass("tsm_bottomLeft");
-    bgElement.append(name).append(enabled).append(connected);
-    $("div.tsm_agent_wrapper").append(bgElement);
+    var bgElement = $("<div>").attr("id", id);
+    var topLeft = $("<div>").addClass("tsm_topLeft");
+    var topRight = $("<div>").addClass("tsm_topRight");
+    var bottomLeft = $("<div>").addClass("tsm_bottomLeft");
+    bgElement.append(topLeft).append(bottomLeft).append(topRight);
+    wrapper.append(bgElement);
   }
 
+  topLeft.html(topLeftText);
+  topRight.html(topRightText);
+  bottomLeft.html(bottomLeftText);
+
+  bgElement.removeClass().addClass(color);
+}
+
+var drawAgent = function(agent) {
   var color;
   if (agent.connected) {
     if (agent.enabled) {
@@ -28,49 +36,43 @@ var drawAgent = function(agent) {
   else {
     color = "tsm_gray";
   }
-
-  name.html(agent.name);
-  connected.html(agent.connected? "Connected" : "Disconnected");
-  enabled.html(agent.enabled? "Enabled" : "Disabled");
-  bgElement.removeClass().addClass(color);
+  var topLeft = agent.name;
+  var topRight = agent.connected? "Connected" : "Disconnected";
+  var bottomLeft = agent.enabled? "Enabled" : "Disabled";
+  createOrUpdateElement("tsm_a_" + agent.id, $(".tsm_agent_wrapper"), topLeft, topRight, bottomLeft, color)
 };
 
-var agentIdCallback = function(response) {
-  var agentIds = response.agent;
-
-  for (var i = 0; i < agentIds.length; i++) {
-    $.ajax({
-      url: tcUrl + agentIds[i].href,
-      headers: { Accept:"application/json" },
-      success: drawAgent
-    });
-  }
-}
-
 var drawBuild = function(buildType) {
-  return function(response) {
-    var build = response.build[0];
+  return function(build) {
     if (build.status != "SUCCESS") {
-      var bgElement = $("<div>").attr("id", "tsm_b_" + build.id);
-      var name = $("<div>").addClass("tsm_topLeft");
-      var status = $("<div>").addClass("tsm_topRight");
-      bgElement.append(name).append(status);
-      $("div.tsm_build_wrapper").append(bgElement);
-      name.html(buildType.projectName + " :: " + buildType.name);
-      status.html(build.status);
-      bgElement.removeClass().addClass("tsm_red");
+      var topLeft = buildType.projectName + " :: " + buildType.name;
+      var topRight = build.finishDate + "<br>" + build.statusText;
+      var bottomLeft = build.status;
+      createOrUpdateElement("tsm_b_" + buildType.id, $("div.tsm_build_wrapper"), topLeft, topRight, bottomLeft, "tsm_red");
+    }
+    else {
+      $("div#tsm_b_" + buildType.id).remove();
     }
   };
 }
 
+var getBuildDetails = function(buildType) {
+  return function(response) {
+    $.ajax({
+      url: tcUrl + response.build[0].href,
+      headers: { Accept:"application/json" },
+      success: drawBuild(buildType)
+    });
+  }
+}
+
 var buildTypesCallback = function(response) {
   var buildTypes = response.buildType;
-  console.log(response);
   for (var i = 0; i < buildTypes.length; i++) {
     $.ajax({
       url: tcUrl + "/httpAuth/app/rest/builds/?locator=count:1,canceled:false,running:false,buildType:id:" + buildTypes[i].id,
       headers: { Accept:"application/json" },
-      success: drawBuild(buildTypes[i])
+      success: getBuildDetails(buildTypes[i])
     });
   }
 }
@@ -81,7 +83,18 @@ var downloadAndDisplayBuilds = function() {
     headers: { Accept:"application/json" },
     success: buildTypesCallback
   });
-  //setTimeout(downloadAndDisplayBuilds, 2000);
+  setTimeout(downloadAndDisplayBuilds, 2000);
+}
+
+var agentIdCallback = function(response) {
+  var agentIds = response.agent;
+  for (var i = 0; i < agentIds.length; i++) {
+    $.ajax({
+      url: tcUrl + agentIds[i].href,
+      headers: { Accept:"application/json" },
+      success: drawAgent
+    });
+  }
 }
 
 var downloadAndDisplayAgents = function() {
@@ -93,13 +106,14 @@ var downloadAndDisplayAgents = function() {
   setTimeout(downloadAndDisplayAgents, 2000);
 }
 
-var tcUrl = "http://lon1vci01.int.openbet.com";
+var tcUrl;
 var body = $("body");
 body.empty();
 $("<div>").addClass("tsm_agent_wrapper").appendTo(body);
 $("<div>").addClass("tsm_build_wrapper").appendTo(body);
 
 chrome.runtime.sendMessage({}, function(response) {
+  tcUrl = "http://lon1vci01.int.openbet.com";
   downloadAndDisplayAgents();
   downloadAndDisplayBuilds();
 });
