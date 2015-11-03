ae3347c6604972bd797194448038390635f6a621
jholdstock <jam.jar69@gmail.com>
2015-11-03 09:56:01 +0000

    Make date/time format consistent

--- js/clock.js
+++ js/clock.js
@@ -1,34 +1,27 @@
-function updateClock ( )
-  {
-  var currentTime = new Date ( );
-    var currentHours = currentTime.getHours ( );
-    var currentMinutes = currentTime.getMinutes ( );
-    var currentSeconds = currentTime.getSeconds ( );
+function updateClock() {
+ 	var currentTime = new Date();
+    var currentHours = currentTime.getHours();
+    var currentMinutes = currentTime.getMinutes();
+    var currentSeconds = currentTime.getSeconds();
 
     // Pad the minutes and seconds with leading zeros, if required
-    currentMinutes = ( currentMinutes < 10 ? "0" : "" ) + currentMinutes;
-    currentSeconds = ( currentSeconds < 10 ? "0" : "" ) + currentSeconds;
+    currentHours = (currentHours < 10 ? "0" : "") + currentHours;
+    currentMinutes = (currentMinutes < 10 ? "0" : "") + currentMinutes;
+    currentSeconds = (currentSeconds < 10 ? "0" : "") + currentSeconds;
 
     // Compose the string for display
     var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds;
     
-    $("#tsm_clock").html(getPrettyDate() + " " + currentTimeString);
-    
+    $("#tsm_clock").html(getPrettyDate() + " - " + currentTimeString);
  }
 
-$(document).ready(function()
-{
+$(document).ready(function() {
   updateClock();
   setInterval('updateClock()', 1000);
 });
 
 var getPrettyDate = function() {
-  var monthNames = [
-    "January", "February", "March",
-    "April", "May", "June", "July",
-    "August", "September", "October",
-    "November", "December"
-  ];
+  var monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
   var date = new Date();
   var day = date.getDate();
