ec43c65d8fa9b74cdac46a5119c7b2bfb28026e1
jholdstock <jam.jar69@gmail.com>
2015-11-09 16:15:51 +0000

    improve how project list is displayed (slightly)

--- js/popup-projects.js
+++ js/popup-projects.js
@@ -23,9 +23,8 @@ var projectsCallback = function(a,b,c) {
 		var p = projects[i];
 		var checkbox = $("<input type='checkbox'>").attr("id", "tsm_bk_" + p.id).prop("tsm_pid", p.id);
 	 	var label = $("<label>").attr("for", "tsm_bk_" + p.id).html("" + p.name.replace("<", "&lt;").replace(">", "&gt;"));
-		var pEl = $("<div>").css("background", "rgba(0,120,0,0.1)").attr("id", "tsm_p_" + p.id).append([checkbox, label]);
+		var pEl = $("<div>").attr("id", "tsm_p_" + p.id).append([checkbox, label]);
 		if (p.parentProjectId && $("#tsm_p_" + p.parentProjectId).length) {
-			pEl.css("padding-left", "3rem");
 			$("#tsm_p_" + p.parentProjectId).append(pEl);
 		} else {
 			$("#projectCheckboxes").append(pEl);
